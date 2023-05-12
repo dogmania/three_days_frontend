fa980bb0e246fd964fd668cb582519997c330559
dogmania <hj296914@gmail.com>
2023-05-13 02:01:01 +0900

    Type: feat/ 회원정보 입력 페이지 생성
    Body: 회원정보 입력 페이지를 구현. 아직 기능은 추가되지 않았음

--- app/src/main/java/com/example/threedays/FirstPageActivity.kt
+++ app/src/main/java/com/example/threedays/FirstPageActivity.kt
@@ -1,11 +1,21 @@
 package com.example.threedays
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.threedays.databinding.ActivityFirstPageBinding
 
 class FirstPageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFirstPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first_page)
+        binding = ActivityFirstPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStartKakao.setOnClickListener {
+            val intent = Intent(this, memberInformationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
