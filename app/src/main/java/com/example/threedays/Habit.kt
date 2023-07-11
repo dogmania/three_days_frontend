b32b14c845bfc39c34023ec7ea529477c548a588
dogmania <hj296914@gmail.com>
2023-07-12 01:56:02 +0900

    습관 인증에 대한 데이터를 저장하기 위해 HabitCertification 데이터 클래스 정의

--- app/src/main/java/com/example/threedays/Habit.kt
+++ app/src/main/java/com/example/threedays/Habit.kt
@@ -4,6 +4,7 @@ data class Habit (
     val period : Int? = null,
     val habitName : String? = null,
     val disclosure : Boolean? = null,
+    val certification: MutableList<HabitCertification>?,
     val achievementRate : Int = 0,
     val combo : Int = 0,
     val numOfAchievement : Int = 0
---
+++ app/src/main/java/com/example/threedays/HabitCertification.kt
@@ -0,0 +1,9 @@
+package com.example.threedays
+
+import android.net.Uri
+
+data class HabitCertification(
+    val image: List<Uri>?,
+    val habitReview: String?,
+    val grade: Int?
+)
