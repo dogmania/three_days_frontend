09addd77ad30e6c8ac315bf2f391e879e7ca2024
dogmania <hj296914@gmail.com>
2023-07-28 23:40:58 +0900

    이미지 url을 실제 이미지로 변환하는 로직 구현

--- app/src/main/java/com/example/threedays/ImageLoadTask.kt
+++ app/src/main/java/com/example/threedays/ImageLoadTask.kt
@@ -1,4 +1,33 @@
 package com.example.threedays
 
-class ImageLoadTask {
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
+import android.widget.ImageView
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+
+class ImageLoadTask(private val imageView: ImageView) : AsyncTask<String, Void, Bitmap>() {
+
+    override fun doInBackground(vararg urls: String): Bitmap? {
+        try {
+            val url = URL(urls[0])
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.doInput = true
+            connection.connect()
+            val input: InputStream = connection.inputStream
+            return BitmapFactory.decodeStream(input)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    override fun onPostExecute(result: Bitmap?) {
+        if (result != null) {
+            imageView.setImageBitmap(result)
+        }
+    }
 }
