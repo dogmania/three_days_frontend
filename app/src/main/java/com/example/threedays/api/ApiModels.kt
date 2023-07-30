f11147ef89b742b7a963aedbe5e130a9d9287113
dogmania <hj296914@gmail.com>
2023-07-30 21:05:26 +0900

    api 코드 추가

--- app/src/main/java/com/example/threedays/api/ApiModels.kt
+++ app/src/main/java/com/example/threedays/api/ApiModels.kt
@@ -1,5 +1,8 @@
 package com.example.threedays.api
 
+import com.google.android.datatransport.runtime.dagger.multibindings.IntoMap
+import java.time.LocalDateTime
+
 data class TokenRequest(
     val accessToken: String,
     val refreshToken: String
@@ -10,19 +13,27 @@ data class TokenResponse(
     val refreshToken: String
     )
 
-data class UserData(
-    val email: String,
-    val nickname: String
+data class Habit(
+    val achievementCount: Int,
+    val achievementRate: Int,
+    val comboCount: Int,
+    val duration: Int,
+    val id: Long,
+    val title: String,
+    val visible: Boolean,
+    var isChecked: Boolean
 )
 
-data class Habit(
-    var achievementCount: Int,
-    var achievementRate: Float,
-    var comboCount: Int,
-    var duration: Int,
-    var id: Int,
-    var title: String,
-    var visible: Boolean
+data class ModifyFragmentHabit(
+    val achievementCount: Int,
+    val achievementRate: Int,
+    val comboCount: Int,
+    val duration: Int,
+    val id: Long,
+    val stopDate: String?,
+    val title: String,
+    val visible: Boolean,
+    var isChecked: Boolean
 )
 
 data class CreateHabit(
@@ -36,4 +47,67 @@ data class UserInfo(
     val email: String,
     val keywords: List<String>,
     val nickname: String
+)
+
+data class KaKaoTokenResponse(
+    val email: String,
+    val nickname: String,
+    val tokenDto: TokenResponse,
+    val userId: Long
+)
+
+data class EditHabit(
+    val title: String,
+    val duration: Int,
+    val visible: Boolean
+)
+
+data class CertifyData(
+    val level: Int,
+    val review: String
+)
+
+data class ProfileFeedResponse(
+    val followerCount: Int,
+    val habitList: List<CertifiedHabit>,
+    val kakaoImageUrl: String,
+    val keywords: List<String>,
+    val nickname: String,
+    val totalAchievementRate: Int,
+    val totalHabitCount: Int,
+    var isFollowing: Boolean
+)
+
+data class CertifiedHabit(
+    val certifyDtos: List<Certification>,
+    val createdHabit: String,
+    val habitId: Long,
+    val title: String
+)
+
+data class Certification(
+    val certifiedDate: String,
+    val certifyId: Long,
+    val imagUrls: List<String>,
+    val level: Int,
+    val review: String
+)
+
+data class UserCertifiedHabit(
+    val certifiedDate: String,
+    val certifyImages: List<String>,
+    val createdHabit: String,
+    val kakaoImageUrl: String,
+    val level: Int,
+    val nickname: String,
+    val review: String,
+    val title: String,
+    val userId: Long
+)
+
+data class SearchedUser(
+    val kakaoProfileUrl: String,
+    val nickname: String,
+    val title: String,
+    val userId: Long
 )
--- app/src/main/java/com/example/threedays/api/ApiService.kt
+++ app/src/main/java/com/example/threedays/api/ApiService.kt
@@ -1,9 +1,8 @@
 package com.example.threedays.api
 
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.http.*
 
 interface ApiService {
     @POST("/api/token")
@@ -18,6 +17,51 @@ interface ApiService {
     @GET("/api/habits")
     suspend fun getHabits(@Query("email") email: String) : MutableList<Habit> //습관 리스트를 가져옴
 
+    @GET("/api/habits/edit-list")
+    suspend fun getHabitEditList(@Query("email") email: String): MutableList<ModifyFragmentHabit>
+
     @POST("/api/login")
-    suspend fun getJwtToken(@Body kakaoAuthToken: String): TokenResponse //인가코드를 전달했을 때 jwt 토큰 반
+    suspend fun getJwtToken(@Body kakaoAuthToken: String): KaKaoTokenResponse //인가코드를 전달했을 때 jwt 토큰 반
+
+    @DELETE("/api/habits/{habitId}")
+    suspend fun deleteHabit(@Path("habitId") habitId: Long)
+
+    @PUT("/api/habits/{habitId}/stop")
+    suspend fun stopHabit(@Path("habitId") habitId: Long)
+
+    @PUT("/api/habits/{habitId}/edit")
+    suspend fun updateHabit(
+        @Path("habitId") habitId: Long,
+        @Body editHabit: EditHabit
+    )
+
+    @Multipart
+    @POST("/api/habits/{habitId}/certify")
+    suspend fun certifyHabit(
+        @Path("habitId") habitId: Long,
+        @Part("level") level: RequestBody,
+        @Part("review") review: RequestBody,
+        @Part images: List<MultipartBody.Part>?
+    )
+
+    @GET("/api/feed/profile/me")
+    suspend fun getMyProfileFeed(@Query("email") email: String): ProfileFeedResponse
+
+    @GET("/api/feed/users")
+    suspend fun getUserFeed(@Query("email") email: String): List<UserCertifiedHabit>
+
+    @GET("/api/search")
+    suspend fun searchUser(@Query("habitTitle") habitTitle: String): List<SearchedUser>
+
+    @GET("/api/feed/profile/{userId}")
+    suspend fun getUserProfile(
+        @Path("userId") userId: Long,
+        @Query("email") email: String
+    ): ProfileFeedResponse
+
+    @POST("/api/users/{fromUserId}/follow/{toUserId}")
+    suspend fun follow(@Path("fromUserId") id: Long, @Path("toUserId") userId: Long)
+
+    @DELETE("/api/users/{fromUserId}/unfollow/{toUserId}")
+    suspend fun unFollow(@Path("fromUserId") id: Long, @Path("toUserId") userId: Long)
 }
