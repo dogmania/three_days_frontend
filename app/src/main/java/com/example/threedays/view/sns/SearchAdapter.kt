aaa5f3013e2830df3243ba87d6db51ea0b9281fb
dogmania <hj296914@gmail.com>
2023-08-01 19:50:37 +0900

    검색 페이지 구현

--- app/src/main/java/com/example/threedays/view/sns/SearchAdapter.kt
+++ app/src/main/java/com/example/threedays/view/sns/SearchAdapter.kt
@@ -1,4 +1,82 @@
 package com.example.threedays.view.sns
 
-class SearchAdapter {
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.request.RequestOptions
+import com.example.threedays.api.SearchedUser
+import com.example.threedays.databinding.ItemSearchBinding
+
+class SearchAdapter(private val userList: List<SearchedUser>, private val searchedTitle: String)
+    : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
+    private var onItemClickListener: OnItemClickListener? = null
+
+    interface OnItemClickListener{
+        fun onItemClick(id: Long)
+    }
+
+    inner class SearchViewHolder(binding: ItemSearchBinding): RecyclerView.ViewHolder(binding.root) {
+        val profileImage = binding.profileImage
+        val name = binding.name
+        val habitTitle = binding.habitTitle
+
+        val root = binding.root
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        val binding = ItemSearchBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return SearchViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        val searchData = userList[position]
+
+        holder.name.text = searchData.nickname
+        holder.habitTitle.text = highlightKeyword(searchData.title, searchedTitle)
+        Glide.with(holder.itemView.context)
+            .load(searchData.kakaoProfileUrl)
+            .apply(RequestOptions.bitmapTransform(CircleCrop()))
+            .into(holder.profileImage)
+
+        holder.root.setOnClickListener {
+            onItemClickListener?.onItemClick(searchData.userId)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        onItemClickListener = listener
+    }
+
+    fun highlightKeyword(text: String, keyword: String): SpannableStringBuilder {
+        val spannableBuilder = SpannableStringBuilder(text)
+
+        val startIndex = text.indexOf(keyword, ignoreCase = true)
+        if (startIndex != -1) {
+            val endIndex = startIndex + keyword.length
+
+            // 검색한 단어의 색상을 변경할 스팬을 생성
+            val colorSpan = ForegroundColorSpan(Color.BLACK)
+
+            // 스팬을 적용하여 검색한 단어만 색상을 변경
+            spannableBuilder.setSpan(
+                colorSpan,
+                startIndex,
+                endIndex,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
+        return spannableBuilder
+    }
 }
--- app/src/main/java/com/example/threedays/view/sns/SearchFragment.kt
+++ app/src/main/java/com/example/threedays/view/sns/SearchFragment.kt
@@ -1,19 +1,72 @@
 package com.example.threedays.view.sns
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.threedays.GlobalApplication
+import com.example.threedays.MainActivity
 import com.example.threedays.R
+import com.example.threedays.databinding.FragmentSearchBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
+    private lateinit var searchAdapter: SearchAdapter
+    private lateinit var habitTitle: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
 
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSearch.setOnClickListener {
+            setRecyclerView()
+        }
+    }
+
+    private fun setRecyclerView() {
+        val app = activity?.application as GlobalApplication
+
+        habitTitle = binding.habitTitle.text.toString()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = app.apiService.searchUser(habitTitle)
+
+                withContext(Dispatchers.Main) {
+                    searchAdapter = SearchAdapter(response, habitTitle)
+                    binding.recyclerView.adapter = searchAdapter
+                    binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+                    searchAdapter.setOnItemClickListener(object: SearchAdapter.OnItemClickListener {
+                        override fun onItemClick(id: Long) {
+                            val userFragment = UserFragment()
+                            val args = Bundle()
+                            args.putLong("userId", id)
+                            userFragment.arguments = args
+
+                            val activity = requireActivity() as MainActivity
+                            activity.replaceFragment(userFragment, "userFragment")
+                        }
+                    })
+                }
+            } catch (e: Exception) {
+                Log.e("SearchFragment", "Error during searchUser API call", e)
+            }
+        }
     }
 }
